43443bd317cbf441447f90107ebc8b903b5046be
Camille Islasse <c.islasse@arobases.fr>
2023-09-27 09:10:34 +0200

    add all var for facebook

--- src/Model/ChannelInterface.php
+++ src/Model/ChannelInterface.php
@@ -9,6 +9,17 @@ namespace Arobases\SyliusInstagramPlugin\Model;
 interface ChannelInterface
 {
     public function getInstagramToken(): ?string;
-
     public function setInstagramToken(?string  $instagramToken): void;
+    public function getAccessToken(): ?string;
+    public function setAccessToken(?string $accessToken): void;
+    public function getUserId(): ?string;
+    public function setUserId(?string $userId): void;
+    public function getUsername(): ?string;
+    public function setUsername(?string $username): void;
+    public function setFaceBookAppSecret(?string $faceBookAppSecret): void;
+    public function getFaceBookAppSecret(): ?string;
+    public function setFaceBookAppId(?string $faceBookAppId): void;
+    public function getFaceBookAppId(): ?string;
+    public function getRedirectUrl(): ?string;
+    public function setRedirectUrl(?string $redirectUrl): void;
 }
